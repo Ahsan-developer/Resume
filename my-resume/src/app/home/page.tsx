ebc53965b213943fac2aac6e6d6f3369baf9e941
Ahsan-developer <rizwan-zaheer@sendaride.com>
2023-03-25 06:47:12 -0500

    basic design of home section is complete. media query not implemented

--- my-resume/src/app/home/page.tsx
+++ my-resume/src/app/home/page.tsx
@@ -9,42 +9,58 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <section id="home">
-      <div>
-        <div>
-          <div>
-            <h1>Hi, I'm a Freelancer</h1>
-            <h2>
+    <section id="home" className="min-h-screen py-[3rem] w-full bg-c1 ">
+      <div className=" w-[80%]  mr-auto ml-auto py-[1.5rem] px-[1.5rem]">
+        <div className="flex w-full">
+          <div className=" col-span-7 visible text-left order-0 self-center  w-full">
+            <h1 className=" font-light text-5xl leading-[1.3]">
+              Hi, I'm a Freelancer
+            </h1>
+            <h2 className=" font-semibold uppercase leading-[1.3] text-8xl">
               <div>
-                <span></span>
+                <span>Developer</span>
                 <span></span>
               </div>
             </h2>
-            <p></p>
-            <Link href={"#"}>View My Works</Link>
-            <Link href={"#"}>
+            <p className=" text-lg leading-[1.8] mb-[1rem]">
+              based in Islamabad, Pakistan.
+            </p>
+            <Link
+              href={"#"}
+              className=" cursor-pointer shadow-[0 5px 15px] shadow-black border-2 font-medium py-[0.8rem] px-[2.6rem] mt-[1rem] bg-black text-white round"
+            >
+              View My Works
+            </Link>
+            <Link
+              href={"#"}
+              className=" cursor-pointer underline border-transparent font-medium py-[0.8rem] px-[2.6rem] mt-[1rem]  text-black round"
+            >
               Contact Me
-              <span>
+              <span className=" font-medium text-lg ml-[0.5rem]">
                 <FontAwesomeIcon
                   icon={faArrowAltCircleDown}
                   className="origin-center"
                 />
               </span>
             </Link>
+
+            <div></div>
           </div>
-          <div></div>
-        </div>
-        <div className=" flex mb-[1.5rem] self-center mt-[0] pl-[0.75rem] pr-[0.75rem] w-[80%]">
-          <div className=" visible rounded-full w-full p-[1rem] inline-block">
-            <img
-              className="rounded-full w-auto h-auto block"
-              alt="Muhammad Ahsan Riaz"
-              src={"/profile-image.png"}
-            />
+          <div className="col-span-5 flex self-center mt-[0] pl-[0.75rem] pr-[12px] max-w-full">
+            <div className=" visible rounded-full w-full p-[1rem] inline-block">
+              <img
+                className="rounded-full w-auto h-auto block border-[14px] border-white"
+                alt="Muhammad Ahsan Riaz"
+                src={"/profile-image.png"}
+              />
+            </div>
           </div>
         </div>
       </div>
-      <Link href={"#about"}>
+      <Link
+        href={"#about"}
+        className=" cursor-pointer relative bottom-1/4 h-[32px] text-xl left-[50%] w-[32px]"
+      >
         <FontAwesomeIcon icon={faArrowDown} className="origin-center" />
       </Link>
     </section>
